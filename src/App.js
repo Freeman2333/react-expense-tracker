5dc255ffc78738f858ccaa8f6e48e00f185a73f5
freeman <freeman23@bigmir.net>
2020-02-29 11:23:21 +0200

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./comps/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
