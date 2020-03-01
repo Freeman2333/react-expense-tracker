5010cd8eb7730cb8177497004b8abd868bbeb8ef
freeman <freeman23@bigmir.net>
2020-03-01 16:32:30 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Header } from "./comps/Header";
+import { Balance } from "./comps/Balance";
+import { IncomeExpenses } from "./comps/IncomeExpenses";
+import { TransactionList } from "./comps/TransactionList";
+import { AddTransaction } from "./comps/AddTransaction";
 import "./App.css";
-
+import { GlobalProvider } from "./context/GlobalState";
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
-    </div>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
